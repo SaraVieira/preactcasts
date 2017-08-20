7ae1bb6ae175f279ef553b310c121d2fe3bf4ca8
Sara Vieira <sara.vieira@mindera.com>
2017-08-20 18:45:26 +0100

    scratch

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": ["prettier"],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "prettier"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import {h} from 'preact'
+
+const App = () => (
+    <div>
+        welcome my friends to preact,
+        next lesson we shall make some routes
+    </div>
+)
+
+export default App
